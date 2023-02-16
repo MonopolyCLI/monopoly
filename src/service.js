77a83ccbb64730691b8280c11d8a1fbd339ef5e8
William Blankenship <code@blankenship.io>
2023-02-16 15:42:24 -0700

    Dont inject NODE_ENV into other services envs

--- src/service.js
+++ src/service.js
@@ -31,6 +31,9 @@ class Resource {
     const result = {};
     for (let i = 0; i < keys.length; i++) {
       const key = keys[i];
+      if (key === "NODE_ENV") {
+        continue; // Skip node_env for other services
+      }
       const value = config[key];
       result[formatEnvKey(`${name}_${key}`)] = value;
     }
